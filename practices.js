aca3846e7d093fefa1084d0e0e73cbef1f6fdf24
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-06 21:28:37 +0600

    Function for Inch to Feet

---
+++ practices.js
@@ -0,0 +1,12 @@
+// inch to feet
+/*
+ * function inchToFeet(inch) {
+  *const feet = parseInt(inch / 12);
+  *const left = inch % 12;
+  *const result = feet + " " + "feet" + " " + left + " " + "inch";
+
+  *return result;
+*}
+
+*console.log(inchToFeet(30));
+ */
