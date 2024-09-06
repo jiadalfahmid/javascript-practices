7280f75555954cb955e37abc9966fa3f86fee729
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-06 22:01:16 +0600

    Leap year calculating function

--- practices.js
+++ practices.js
@@ -10,3 +10,18 @@
 
 *console.log(inchToFeet(30));
  */
+// -------------------------------------------------
+
+// Determining Leap Years
+// To determine if a year is a leap year, we apply a simple rule: if the year is divisible by 4, it's a leap year, except for end-of-century years, which must also be divisible by 400.
+/*
+*function isLeapYear(year) {
+*  if (year % 4 === 0 && year % 400 === 0) {
+*    return true;
+*  }
+*  return false;
+*}
+
+*const yearIs = 2052;
+*console.log(isLeapYear(yearIs));
+*/
