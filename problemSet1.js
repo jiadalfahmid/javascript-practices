1becbcd21efe1ed86340d53450b2d98dc294e2c3
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-08 13:29:50 +0600

    Repeat Counter

--- problemSet1.js
+++ problemSet1.js
@@ -1,11 +1,30 @@
 // Write a function to convert temperature from Celsius to Fahrenheit.
 /*
-*function celsiusToFahrenheit(celsius) {
-*  let fahrenheit = celsius * (9 / 5) + 32;
-*  return fahrenheit;
+ *function celsiusToFahrenheit(celsius) {
+ *  let fahrenheit = celsius * (9 / 5) + 32;
+ *  return fahrenheit;
+ *}
+ *
+ *const celsius = 32;
+ *let fahrenheit = celsiusToFahrenheit(celsius);
+ *console.log('the temperature is: ',fahrenheit,'°F')
+ */
+
+// Count how many times the a number is repeated in the array.
+/*
+*let numbers = [5, 6, 11, 12, 98, 5];
+*
+*function countRepeat(array, find) {
+*  let count = 0;
+*  for (let num of array) {
+*    if (find === num) {
+*      count++;
+*    }
+*  }
+*  return count;
 *}
 *
-*const celsius = 32;
-*let fahrenheit = celsiusToFahrenheit(celsius);
-*console.log('the temperature is: ',fahrenheit,'°F')
- */
+*let find = 13;
+*let ifRepeated = countRepeat(numbers, find);
+*console.log(ifRepeated);
+*/
