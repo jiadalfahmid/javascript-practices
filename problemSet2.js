2c81f9dffd08ca64192b8ef837383c865b056ff1
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-08 21:50:12 +0600

    Average price of phone

--- problemSet2.js
+++ problemSet2.js
@@ -57,3 +57,25 @@
  */
 
 // average price of phone
+
+const phones = [
+ { model: "PhoneA", brand: "Iphone", price: 95000 },
+ { model: "PhoneB", brand: "Samsung", price: 40000 },
+ { model: "PhoneC", brand: "Oppo", price: 26000 },
+ { model: "PhoneD", brand: "Nokia", price: 35000 },
+ { model: "PhoneE", brand: "Iphone", price: 105000 },
+ { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+function avgPrice(phones)
+{
+ sum=0;
+ for(const phone of phones){
+  sum+=phone.price;
+ }
+ avg= sum / phones.length
+ return avg;
+}
+
+let price = avgPrice(phones)
+console.log(price)
